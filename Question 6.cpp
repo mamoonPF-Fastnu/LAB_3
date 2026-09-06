8503deb5d6c3f55a3b232c22cdc2e7a6591fac0a
mamoonPF-Fastnu <mamoonahmed1122@gmail.com>
2026-09-06 22:50:18 +0500

    Create Question 6.cpp
    /t is table space use to space words at table space to make it look tabular and \" text \" to print actual quotations

---
+++ Question 6.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("Welcome to C Programming\n\n");
+
+    printf("Name:\tAli\n\n");
+    printf("Age:\t20\n\n");
+    printf("Grade:\tA\n\n");
+
+    printf("File Path: C:\\Users\\Student\n");
+
+    
+    printf("He said:\"Hello\"");
+
+    return 0;
+}
