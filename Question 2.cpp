99feee4014f5e52df246f0c3408475075c7c4f33
mamoonPF-Fastnu <mamoonahmed1122@gmail.com>
2026-09-06 21:28:34 +0500

    Making programs in Dev C++ running them there and copying them in VS to put on Github and local directorary named same as github account/repo

---
+++ Question 2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int first, second;
+
+    printf("Enter first number: ");
+    scanf("%d", &first);
+
+    printf("Enter second number: ");
+    scanf("%d", &second);
+
+    printf("First Number: %d\n", first);
+    printf("Second Number: %d\n", second);
+
+    return 0;
+}
